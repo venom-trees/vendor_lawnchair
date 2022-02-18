8f62f7a9f563d1df3eaa8a478a751ca2a225391f
Ziauddin Sameer <ziauddinsameer@gmail.com>
2022-02-18 08:55:01 +0000

    lawnchair: Update libs

--- lawnchair.mk
+++ lawnchair.mk
@@ -3,7 +3,8 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/lawnchair
 
 PRODUCT_COPY_FILES += \
-    vendor/lawnchair/libs/libnrb.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libnrb.so
+    vendor/lawnchair/lib/libnrb.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/libnrb.so \
+    vendor/lawnchair/lib64/libnrb.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libnrb.so
 
 PRODUCT_PACKAGES += \
     Lawnchair \
